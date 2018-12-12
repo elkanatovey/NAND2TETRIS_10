bc024c4fb6f858a4451b86240178488b52f0b8a9
elkanatovey <elkanatovey@gmail.com>
2018-12-12 13:50:25 +0200

    start + start file collect

--- parser.py
+++ parser.py
@@ -3,27 +3,31 @@
 # WRITER : Aviad Dudkewitz, Elkana Tovey
 # DESCRIPTION: This program translate VM code to Hack assembly language.
 ##############################################################################
+import sys
+import re
+from os import listdir
+from os.path import isfile, isdir
 
 INVALID_ARGS = "The file given as input is invalid..."
-
+NUMBER_OF_ARGS = 2
 
 VALID_INPUT_SUFFIX = ".*\.jack$"
-vm_suffix_pattern = re.compile(VALID_INPUT_SUFFIX)
+jack_suffix_pattern = re.compile(VALID_INPUT_SUFFIX)
 
 
 
 def get_files(args):
     """
     :param args: the arguments given to the program.
-    :return: the list of paths to .vm files
+    :return: the list of paths to .jack files
     """
     list_of_files_path = []
     if len(args) == NUMBER_OF_ARGS:
-        if isfile(args[1]) and vm_suffix_pattern.match(args[1]):
+        if isfile(args[1]) and jack_suffix_pattern.match(args[1]):
             list_of_files_path.append(args[1])
         elif isdir(args[1]):
             for file in listdir(args[1]):
-                if vm_suffix_pattern.match(file):
+                if jack_suffix_pattern.match(file):
                     list_of_files_path.append(args[1] + "/" + file)
         return list_of_files_path
     else:
