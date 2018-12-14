059334895fa6bb85cf46d6998faff58bf36033f1
aviad dudkevich <aviad.dudkevich@mail.huji.ac.il>
2018-12-14 13:10:26 +0200

    aviad finish patterns

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -1,6 +1,18 @@
 import re
 
 COMMENT = "(//.*)|(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)"
+EMPTY_TEXT_PATTERN = re.compile("\s*")
+KEY_WORD_PATTERN = re.compile("^\s*("
+                              "class|constructor|function|method|static|field"
+                              "|var|int|char|booolean|void|true|false|null|this|"
+                              "let|do|if|else|while|return)\s*")
+SYMBOL_PATTERN = re.compile("^\s*({|}|\(|\)|\[|\]|\.|,"
+                            "|;|\+|-|\*|/|&|\||<|>|=|~)\s*")
+DIGIT_PATTERN = re.compile("^\s*(\d+)\s*")
+STRING_PATTERN = re.compile("^\s*\"(.*)\"\s*")
+IDENTIFIER_PATTERN = re.compile("^\s*([a-zA-Z_][a-zA-Z1-9_]*)\s*")
+
+
 DEBUGGING = True
 
 class JackTokenizer:
@@ -25,6 +37,8 @@ class JackTokenizer:
         """
         with open(input_file_path, "r") as file:
             self.text = file.read()
+        if DEBUGGING:
+            print(self.text)
         self.clear_all_comments()
         self.tokenType = "NONE"
         self.currentToken = "NONE"
@@ -36,14 +50,15 @@ class JackTokenizer:
         self.text = re.sub(COMMENT, "", self.text)
         print(self.text)
 
-
-
-
     def hasMoreTokens(self):
-        pass
+        if re.fullmatch(EMPTY_TEXT_PATTERN, self.text):
+            return False
+        else:
+            return True
 
     def advance(self):
-        self.skipWhiteSpace()
+        if self.hasMoreTokens():
+            pass
 
 
     def tokenType(self):
@@ -66,3 +81,4 @@ class JackTokenizer:
 
 if __name__ == "__main__" and DEBUGGING:
     a = JackTokenizer("Square\Square.jack")
+    print(a.hasMoreTokens())
